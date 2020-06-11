2adc4054526ec315d53d94a5b0375f9f8196c252
oldrookie-engineer <paburofu0023@icloud.com>
2020-06-11 15:09:34 +0900

    kindergartensテーブルを作成

--- app/controllers/kindergartens_controller.rb
+++ app/controllers/kindergartens_controller.rb
@@ -2,4 +2,24 @@ class KindergartensController < ApplicationController
   def index
     @kindergartens = Kindergartens.all
   end
+
+  def new
+    @kindergarten = Kindergarten.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- db/migrate/20200611053537_create_kindergartens.rb
+++ db/migrate/20200611053537_create_kindergartens.rb
@@ -1,7 +1,12 @@
 class CreateKindergartens < ActiveRecord::Migration[5.2]
   def change
     create_table :kindergartens do |t|
-
+      t.string :name, null: false
+      t.string :address, null: false
+      t.integer :phone_number, null: false
+      t.string :image
+      t.float :latitude, null: false
+      t.float :longitude, null: false
       t.timestamps
     end
   end
