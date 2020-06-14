0ca4da918c79206ad5a3189621d64c77666da9fa
oldrookie-engineer <paburofu0023@icloud.com>
2020-06-14 15:45:57 +0900

    carrierwaveとmini_magickをinstall

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'devise-i18n'
 gem 'ransack'
+gem 'carrierwave'
+gem 'mini_magick'
--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,4 +1,12 @@
 class Authorization < ApplicationRecord
   belongs_to :kindergarten
-  validates :principal, presence: true
+  with_options presence: true do
+    validates :principal
+    validates :installation_date
+    validates :number_of_classes
+    validates :capacity
+    validates :number_of_students
+    validates :school_area
+    validates :school_floor_area
+  end
 end
