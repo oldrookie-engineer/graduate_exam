acb73eb8d763c7f262ffd264a3552d78d88afdfd
oldrookie-engineer <paburofu0023@icloud.com>
2020-06-14 15:59:17 +0900

    外観写真アップロード機能を搭載

--- app/models/kindergarten.rb
+++ app/models/kindergarten.rb
@@ -1,11 +1,14 @@
 class Kindergarten < ApplicationRecord
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true, format: {with: /\A[0-9]{2,3}-[0-9]{1,4}-[0-9]{4}\z/}
-  validates :latitude, presence: true
-  validates :longitude, presence: true
+  with_options presence: true do
+    validates :name
+    validates :address
+    validates :phone_number, format: {with: /\A[0-9]{2,3}-[0-9]{1,4}-[0-9]{4}\z/}
+    validates :latitude
+    validates :longitude  
+  end
   has_many :stations, dependent: :destroy
   has_one :authorization, dependent: :destroy
   accepts_nested_attributes_for :stations, allow_destroy: true
   accepts_nested_attributes_for :authorization, allow_destroy: true
+  mount_uploader :image, ImageUploader
 end
