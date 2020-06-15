fa30219ce8305cac800e1f0fcfcf05747993ebee
oldrookie-engineer <paburofu0023@icloud.com>
2020-06-15 15:46:38 +0900

    tasksコントローラーとテーブルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # とりあえずのroot設定
   root 'kindergartens#index'
   resources :kindergartens
+  namespace :api, format: 'json' do
+    resources :tasks, only:%i(index create show update destroy)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_14_083141) do
+ActiveRecord::Schema.define(version: 2020_06_15_063822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 2020_06_14_083141) do
     t.datetime "updated_at", null: false
     t.bigint "kindergarten_id"
     t.index ["kindergarten_id"], name: "index_stations_on_kindergarten_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
