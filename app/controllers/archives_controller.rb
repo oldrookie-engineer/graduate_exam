18b82bbfa77c584bb0d6f23a928b59c467f2f9f5
oldrookie-engineer <paburofu0023@icloud.com>
2020-06-21 15:01:48 +0900

    アーカイブのCRUD機能を実装

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,2 +1,45 @@
 class ArchivesController < ApplicationController
+  before_action :set_archive, only: [:edit, :update, :destroy]
+  def index
+    @q = Archive.ransack(params[:q])
+    @archives = @q.result(distinct: true)
+  end
+
+  def new
+    @archive = Archive.new
+  end
+
+  def create
+    @archive = Archive.new(archive_params)
+    if @archive.save
+      redirect_to archives_path, notice: 'アーカイブを作成しました！'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @archive.update(archive_params)
+      redirect_to archives_path, notice: 'アーカイブを編集しました！'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @archive.destroy
+    redirect_to archives_path, notice: 'アーカイブを削除しました！'
+  end
+
+  private
+  def archive_params
+    params.require(:archive).permit(:name, :title, :installation_date, :status_of_use)
+  end
+
+  def set_archive
+    @archive = Archive.find(params[:id])
+  end
 end
