5de809efb8c5f1df302fdfcaa882b3a64bbf7f35
oldrookie-engineer <paburofu0023@icloud.com>
2020-06-30 19:43:37 +0900

    oauth用のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   end
   devise_for :users, controllers: {
     # sessions: 'users/sessions'
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
   resources :users
   resources :kindergartens
