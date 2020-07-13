d11ee06278a62ac4a6844f308b72ba7b360359f7
oldrookie-engineer <paburofu0023@icloud.com>
2020-07-13 16:59:44 +0900

    エラーページを実装

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,4 +1,10 @@
 class Archive < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :title
+    validates :installation_date
+    validates :status_of_use
+  end
   enum status_of_use: {
     在庫あり: 0,
     使用中: 1
