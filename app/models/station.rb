0b5dabc42d30d03c1aa2ca612f8d8b45d935e03f
oldrookie-engineer <paburofu0023@icloud.com>
2020-07-17 16:11:15 +0900

    StationモデルのRSpecテストをクリア

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,6 +1,8 @@
 class Station < ApplicationRecord
-  validates :route, presence: true
-  validates :station_name, presence: true
-  validates :walk_time, presence: true
+  with_options presence: true do
+    validates :route
+    validates :station_name
+    validates :walk_time
+  end
   belongs_to :kindergarten
 end
---
+++ spec/models/station_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+RSpec.describe Station, type: :model do
+  it 'routeが空ならバリデーションが通らない' do
+    station = Station.new(route: '')
+    expect(station).not_to be_valid
+  end
+  it 'station_nameが空ならバリデーションが通らない' do
+    station = Station.new(station_name: '')
+    expect(station).not_to be_valid
+  end
+  it 'walk_timeが空ならバリデーションが通らない' do
+    station = Station.new(walk_time: '')
+    expect(station).not_to be_valid
+  end
+end
