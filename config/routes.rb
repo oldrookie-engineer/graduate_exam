6fea3e7eedf8bf8f0db15b05339b25b735095ee5
oldrookie-engineer <paburofu0023@icloud.com>
2020-08-14 19:07:28 +0900

    メッセージ受信確認リンク設定

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,5 @@ Rails.application.routes.draw do
     resources :messages
   end
   resources :archives
-  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
