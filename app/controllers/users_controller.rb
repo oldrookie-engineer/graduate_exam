1945279d399e11d5b55704c22f9b81075a3e8484
oldrookie-engineer <paburofu0023@icloud.com>
2020-08-15 14:49:00 +0900

    登録ユーザー名の検索機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @q = User.ransack(params[:q])
     if current_user.try(:admin?)
-      @users = User.all.page(params[:page]).per(5)
+      # @users = User.all.page(params[:page]).per(5)
+      @users = @q.result(distinct: true).page(params[:page]).per(5)
     else
       @users = User.where(admin: true).page(params[:page]).per(5)
     end
