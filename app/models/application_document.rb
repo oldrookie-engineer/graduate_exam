34cea845ba61c2a40b8d0c88e31c16ee33e7bede
oldrookie-engineer <paburofu0023@icloud.com>
2020-08-17 21:01:52 +0900

    申請処理期限へのマイナス日付入力に対するバリデーションを設定

--- app/models/application_document.rb
+++ app/models/application_document.rb
@@ -6,6 +6,7 @@ class ApplicationDocument < ApplicationRecord
     validates :processing_deadline
     validates :processing
   end
+  validate :date_not_before_today
   belongs_to :user
   scope :deadline, -> {
     where('processing_deadline <= ?', Time.current.next_day(3).end_of_day)
@@ -18,4 +19,8 @@ class ApplicationDocument < ApplicationRecord
     処理中: 1,
     処理済: 2
   }
+
+  def date_not_before_today
+    errors.add(:processing_deadline, "は、本日以降のものを選択してください") if processing_deadline.nil? || processing_deadline < Date.today
+  end
 end
